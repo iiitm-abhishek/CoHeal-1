779fdee238bcaadb970fbfe8bfad70af12bc5834
Abhishek Gautam <88575371+iiitm-abhishek@users.noreply.github.com>
2021-11-24 22:22:35 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -38,44 +38,41 @@ class Login extends React.Component {
 	render(){
 		return(
 			<div>
-				{
-					this.state.showlink?
-						<div>
-							<h2>Logged in Successfully</h2>
-							<Link to='/Citizen' className="f4 blue link dim grow">Go to Citizen Dashoard</Link>
-						</div>
-					:
-						<div className="container">
-							<b className="page_title">Login </b>
-						        <div className="form">
-			            <div className="form_input">
-										<label>
-											<span className="label">UID<b style={{color: "red"}}>*</b>:</span> 
-										    <input type="number" name="uid" id="uid" placeholder="XXXXXXXXXXXX" onChange={this.onUIDChange}/>
-										</label>
-									</div>
-									<div className="form_input">
-										<label>
-											<span className="label">Password<b style={{color: "red"}}>*</b>:</span>
-										    <input type="password" name="password" id="password" onChange={this.onPasswordChange}/>
-										</label>
-									</div>
-								</div>
-								<div className="submit_btn_div">
-									<button onClick={this.onSubmitLogin} type="submit">Login</button>
-								</div>
-								<div className="f5 pt2">
-									New User?<b> </b>
-									<Link to="/Register" onClick={() => this.props.onRouteChange('/Register')}> <p className='link black dim underline dib pointer'>Register Here</p></Link>
-								</div>
-						</div>
-				}
-			</div>
-
-
-			
-		);
-	}
+			      {
+				this.state.showlink?
+				    <div>
+					  <h2>Logged in Successfully</h2>
+					  <Link to='/Citizen' className="f4 blue link dim grow">Go to Citizen Dashoard</Link>
+				    </div>
+				 :
+				    <div className="container">
+					<b className="page_title">Login </b>
+				           <div className="form">
+			                        <div className="form_input">
+						    <label>
+							    <span className="label">UID<b style={{color: "red"}}>*</b>:</span> 
+					                    <input type="number" name="uid" id="uid" placeholder="XXXXXXXXXXXX" onChange={this.onUIDChange}/>
+						    </label>
+						 </div>
+						 <div className="form_input">
+						      <label>
+							     <span className="label">Password<b style={{color: "red"}}>*</b>:</span>
+							     <input type="password" name="password" id="password" onChange={this.onPasswordChange}/>
+						      </label>
+						  </div>
+					     </div>
+					     <div className="submit_btn_div">
+					     	 <button onClick={this.onSubmitLogin} type="submit">Login</button>
+					     </div>
+					     <div className="f5 pt2">
+						  <b> New User?</b>
+						  <Link to="/Register" onClick={() => this.props.onRouteChange('/Register')}> <p className='link black dim underline dib pointer'>Register Here</p></Link>
+					      </div>
+				    </div>
+				 }
+			  </div>	
+		    );
+	  }
 }
 
 export default Login;
