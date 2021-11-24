dcf12d508e6660e88c8946f63221b2d8d5860def
Abhishek Gautam <88575371+iiitm-abhishek@users.noreply.github.com>
2021-11-24 23:28:54 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,8 +9,8 @@ function Banner(){
           <div className="cover bg-left bg-center1">
             <div className="bg-black-80">
               <div className="tc-l ph3">
-                <h1 className="f2 f1-l fw2 white-90 pt6 mt0 lh-title">This is your super impressive headline</h1>
-                <h2 className="fw1 f3 pb6 mb0 white-80">Now a subheadline where explain your wonderful new startup even more</h2>
+                <h1 className="f2 f1-l fw2 white-90 pt6 mt0 lh-title"> Coheal </h1>
+                <h2 className="fw1 f3 pb6 mb0 white-80"> Book Beds and Vaccine Slots Easily </h2>
               </div>
             </div>
           </div> 
